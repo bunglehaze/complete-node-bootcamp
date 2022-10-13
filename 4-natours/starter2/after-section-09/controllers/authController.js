7ed4292f544bcdd2d7f06311e268c965d1459288
bunglehaze <leigh.godson@withyouwithme.com>
2022-10-13 20:58:01 +0100

    cleanup old sections not needed

--- 4-natours/starter2/after-section-09/controllers/authController.js
+++ 4-natours/starter2/after-section-09/controllers/authController.js
@@ -14,25 +14,21 @@ const signToken = id => {
     });
   };
 
-exports.signup = catchAsync(async (req, res, next) => {
-const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt,
+  const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
 
-});
-
-const token = signToken(newUser._id);
-
-    res.status(201).json({
+    res.status(statusCode).json({
     status: 'success',
     token,
     data: {
-    user: newUser
+    user
     }
 });
+}
+
+exports.signup = catchAsync(async (req, res, next) => {
+const newUser = await User.create(req.body);
+createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync (async (req, res, next) => {
@@ -50,6 +46,7 @@ exports.login = catchAsync (async (req, res, next) => {
         return next(new AppError('Invalid email or password', 401));
     }
 
+        createSendToken(user, 200, res);
         const token = signToken(user._id);
         
         res.status(200).json({
@@ -165,9 +162,10 @@ user.passwordConfirm = req.body.passwordConfirm;
 user.passwordResetToken = undefined;
 user.passwordResetExpires = undefined;
 await user.save();
-    //3  update cfhanged passwordAt property for the user
+    //3  update changed passwordAt property for the user
 
 //4 log the user in , send JWT 
+createSendToken(user, 200, res);
 const token = signToken(user._id);
         
         res.status(200).json({
@@ -176,6 +174,20 @@ const token = signToken(user._id);
         });
     });
 
-    exports.updatePassword = (req, res, next) => {
-        
-    }
+    exports.updatePassword = catchAsync(async (req, res, next) => {
+        //1 get the user from collection
+        const user = await User.findById( req.user.id ).select('+password');
+        //2 check if posted current password is correct
+        if(!(user.correctPassword(req.body.passwordCurrent, user.password))) {
+            return next(new AppError('Your current password is incorrect', 401))
+        }
+        //3 if so, update the password
+user.password = req.body.password;
+user.passwordConfirm = req.body.passwordConfirm;
+await user.save();
+
+//user findByIdAndUpdate would not work
+
+        //4 log user in, send jwt
+        createSendToken(user, 200, res);
+    });
--- 4-natours/starter2/after-section-09/routes/userRoutes.js
+++ 4-natours/starter2/after-section-09/routes/userRoutes.js
@@ -10,7 +10,14 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-console.log();
+
+router.patch('/updateMyPassword', 
+authController.protect, 
+authController.updatePassword
+);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 
 router
   .route('/')
